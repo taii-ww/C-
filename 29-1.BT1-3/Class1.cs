c0a51ae098cd5526097ec1b4c3eee61950f4ce22
taii-ww <tait91026@gmail.com>
2026-03-04 21:19:55 +0700

    bài tập 1->3 ngày 29/1

---
+++ 29-1.BT1-3/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29_1.BT1_3
+{
+    internal class Class1
+    {
+       
+            public static string Xuly(String input)
+            {
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    return "Lỗi: Chuỗi rỗng hoặc null!";
+                }
+
+                input = input.Trim();
+                string[] words = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i< words.Length; i++)
+                {
+                    words[i] = words[i].Substring(0, 1).ToUpper() + words[i].Substring(1).ToLower();
+                }
+                return string.Join(" ", words);
+            }
+        
+    }
+}
---
+++ 29-1.BT1-3/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using _29_1.BT1_3;
+using System.Text;
+
+String input = Class2.NhapChuoi();
+String output = Class1.Xuly(input);
+
+Console.WriteLine("Chuẩn hóa: " + output);
+
+Console.WriteLine("Số từ trong chuỗi: " + Class3.Dem(input));
+
+if (Class4.KiemTra(input))
+    Console.WriteLine("Chuỗi đối xứng");
+else Console.WriteLine("Chuỗi ko đối xứng");
