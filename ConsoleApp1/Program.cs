0652e317d5b560499333e314261bcf047aef9ba2
taii-ww <tait91026@gmail.com>
2026-03-17 23:33:37 +0700

    Bài 14

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,9 +10,22 @@ Console.InputEncoding = Encoding.UTF8;
 Console.WriteLine("Trần Đức Tài ");
 Console.WriteLine("Msv: 2415053122241 \n");
 
-Console.WriteLine("Bài 13");
+Console.WriteLine("Bài 14");
 
 
 
-Console.WriteLine("Tổng số sinh viên: " + Student.list.Count);
+Console.Write("Nhập ID cần xóa: ");
+int id = int.Parse(Console.ReadLine());
 
+Student s = Student.list.Find(x => x.Id == id);
+
+if (s != null)
+{
+    Student.list.Remove(s);
+}
+
+Console.Write("Sau khi xóa: ");
+foreach (Student ss in Student.list)
+{
+    Console.WriteLine("Id: " + ss.Id + ", Name: " + ss.Name);
+}
