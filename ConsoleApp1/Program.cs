40529172c5ddd3c9331f5debf9cf36f0b37f62c2
taii-ww <tait91026@gmail.com>
2026-03-17 23:48:10 +0700

    Bài 18

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using ConsoleApp1;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,45 +10,9 @@ Console.InputEncoding = Encoding.UTF8;
 Console.WriteLine("Trần Đức Tài ");
 Console.WriteLine("Msv: 2415053122241 \n");
 
-Console.WriteLine("Bài 17");
-
-List<int> list = new List<int>();
-
-Console.Write("Nhập số lượng: ");
-int n = int.Parse(Console.ReadLine());
-
-for (int i = 0; i < n; i++)
-{
-    Console.Write("Nhập số: ");
-    int x = int.Parse(Console.ReadLine());
-    list.Add(x);
-}
-
-// đếm
-Dictionary<int, int> dict = new Dictionary<int, int>();
-
-foreach (int x in list)
-{
-    if (dict.ContainsKey(x))
-        dict[x]++;
-    else
-        dict[x] = 1;
-}
-
-// tìm max
-int maxCount = 0;
-int result = list[0];
-
-foreach (var item in dict)
-{
-    if (item.Value > maxCount)
-    {
-        maxCount = item.Value;
-        result = item.Key;
-    }
-}
-
-Console.WriteLine("Số xuất hiện nhiều nhất: " + result);
+Console.WriteLine("Bài 18");
 
 
+
+Student.PrintMax();
 
---
+++ ConsoleApp1/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Student
+    {
+        public string Name;
+        public double Score;
+
+        public static List<Student> list = new List<Student>()
+    {
+        new Student { Name = "An", Score = 7.5 },
+        new Student { Name = "Binh", Score = 9.0 },
+        new Student { Name = "Cuong", Score = 8.2 }
+    };
+
+        public static void PrintMax()
+        {
+            double max = list[0].Score;
+
+            foreach (Student s in list)
+            {
+                if (s.Score > max)
+                    max = s.Score;
+            }
+
+            foreach (Student s in list)
+            {
+                if (s.Score == max)
+                {
+                    Console.WriteLine(s.Name + " - " + s.Score);
+                }
+            }
+        }
+    }
+}
