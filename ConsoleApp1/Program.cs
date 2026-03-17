4bad324c77343a76e913bc76d13e9e2503ed0af8
taii-ww <tait91026@gmail.com>
2026-03-17 21:29:36 +0700

    Bài 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+Console.InputEncoding = Encoding.UTF8;
+
+Console.WriteLine("Trần Đức Tài ");
+Console.WriteLine("Msv: 2415053122241 \n");
+
+
+List<int> list = new List<int>();
+
+list.Add(1);
+list.Add(3);
+list.Add(5);
+list.Add(77);
+list.Add(-4);
+
+Console.WriteLine("Bài 1");
+foreach (int x in list)
+{
+    Console.WriteLine(x);
+}
