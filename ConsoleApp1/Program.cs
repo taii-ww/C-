83517970c81b8e20402ce41ddcc7b59878064f4a
taii-ww <tait91026@gmail.com>
2026-03-19 23:14:23 +0700

    Bai 7

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,11 +9,16 @@ Console.WriteLine("Trần Đức Tài");
 Console.WriteLine("Lớp: 24T2");
 Console.WriteLine("Msv: 2415053122241 \n");
 
-Console.WriteLine("Bài 6");
+Console.WriteLine("Bài 7");
 
-List<int> numbers = new List<int>() { 2, 5, 9, -1, -6};
+List<int> numbers = new List<int>() { 2, 5, 9, -1, -6, 12};
 
-int countEven = numbers.Count(x => x % 2 == 0);
+bool a = numbers.Any(x => x > 10);
 
-Console.WriteLine("Số phần tử chẵn là: "+countEven);
+if (a)
+    Console.WriteLine("Có giá trị lớn hơn 10");
+else
+{
+    Console.WriteLine("Không Có giá trị lớn hơn 10");
+}
 
