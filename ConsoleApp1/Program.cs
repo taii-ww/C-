ec714da2870ade4c28f7bcffb1063ea25c733e9b
taii-ww <tait91026@gmail.com>
2026-03-19 23:02:26 +0700

    Bai 5

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,13 +9,13 @@ Console.WriteLine("Trần Đức Tài");
 Console.WriteLine("Lớp: 24T2");
 Console.WriteLine("Msv: 2415053122241 \n");
 
-Console.WriteLine("Bài 4");
+Console.WriteLine("Bài 5");
 
 List<int> numbers = new List<int>() { 2, 5, 9, -1, -6};
 
-var sortedNumbers = numbers.OrderByDescending(x => x);
+var squares = numbers.Select(x => x * x);
 
-foreach (var n in sortedNumbers)
+foreach (var n in squares)
 {
     Console.WriteLine(n);
 }
