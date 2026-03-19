0c22ab364a9d8cd5680152ab0d45ca121e65c2ae
taii-ww <tait91026@gmail.com>
2026-03-19 22:19:48 +0700

    Bai 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,11 +9,11 @@ Console.WriteLine("Trần Đức Tài");
 Console.WriteLine("Lớp: 24T2");
 Console.WriteLine("Msv: 2415053122241 \n");
 
-Console.WriteLine("Bài 1");
+Console.WriteLine("Bài 2");
 
 List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8 };
 
-var evenNumbers = numbers.Where(x => x % 2 == 0);
+var evenNumbers = numbers.Where(x => x > 5);
 
 foreach (var n in evenNumbers)
 {
