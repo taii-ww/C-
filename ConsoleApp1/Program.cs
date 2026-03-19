8b13f732ed2121e7242004868b65d1eea39e33de
taii-ww <tait91026@gmail.com>
2026-03-20 00:25:59 +0700

    Bai 11

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ Console.WriteLine("Trần Đức Tài");
 Console.WriteLine("Lớp: 24T2");
 Console.WriteLine("Msv: 2415053122241 \n");
 
-Console.WriteLine("Bài 10");
+Console.WriteLine("Bài 11");
 
 List<Student> students = new List<Student>()
 {
@@ -20,10 +20,7 @@ List<Student> students = new List<Student>()
     new Student{Id=4, Name="Dung", Score=7}
 };
 
-var a = students.OrderByDescending(x => x.Score);
+var a = students.OrderByDescending(x => x.Score).First();
 
-foreach(var n in a)
-{  
-    Console.WriteLine(n.Score);
-}
+Console.WriteLine(a.Name + " - " + a.Score);
 
