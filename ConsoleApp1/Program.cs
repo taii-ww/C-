5408d39e615c27de36d1bd0383ddba79ab11efdd
taii-ww <tait91026@gmail.com>
2026-03-20 00:38:47 +0700

    Bai 13

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,17 +10,20 @@ Console.WriteLine("Trần Đức Tài");
 Console.WriteLine("Lớp: 24T2");
 Console.WriteLine("Msv: 2415053122241 \n");
 
-Console.WriteLine("Bài 12");
+Console.WriteLine("Bài 13");
 
 List<Student> students = new List<Student>()
 {
     new Student{Id=1, Name ="An", Score=8},
     new Student{Id=2, Name="Binh", Score=6},
-    new Student{Id=3, Name="Chi", Score=9},
-    new Student{Id=4, Name="Dung", Score=7}
+    new Student{Id=3, Name="Chi", Score=7},
+    new Student{Id=4, Name="Dung", Score=7},
 };
 
-int a = students.Count(x => x.Score == 7);
+var a = students.FirstOrDefault(x => x.Score == 7);
 
-Console.WriteLine("Số sinh viên đạt 7đ là: " + a);
+if (a != null)
+{
+    Console.WriteLine(a.Name + " - " + a.Score);
+}
 
