e0737c3e66a26eb29bef96de21f7a576c7a78738
taii-ww <tait91026@gmail.com>
2026-03-20 00:48:37 +0700

    Bai 14

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,20 +10,23 @@ Console.WriteLine("Trần Đức Tài");
 Console.WriteLine("Lớp: 24T2");
 Console.WriteLine("Msv: 2415053122241 \n");
 
-Console.WriteLine("Bài 13");
+Console.WriteLine("Bài 14");
 
 List<Student> students = new List<Student>()
 {
     new Student{Id=1, Name ="An", Score=8},
     new Student{Id=2, Name="Binh", Score=6},
-    new Student{Id=3, Name="Chi", Score=7},
+    new Student{Id=3, Name="Chi", Score=9},
     new Student{Id=4, Name="Dung", Score=7},
 };
 
-var a = students.FirstOrDefault(x => x.Score == 7);
+var a = students.Any(s => s.Score < 5);
 
-if (a != null)
+if (a)
+    Console.WriteLine("Có người rớt");
+else
 {
-    Console.WriteLine(a.Name + " - " + a.Score);
+    Console.WriteLine("Ko có ai rớt");
 }
+
 
