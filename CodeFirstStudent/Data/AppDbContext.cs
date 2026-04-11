f5f5de7a886fc944a1f89ea975593eb37e61c1f7
taii-ww <tait91026@gmail.com>
2026-04-11 21:33:16 +0700

    Apply migration and create database

--- CodeFirstStudent/Data/AppDbContext.cs
+++ CodeFirstStudent/Data/AppDbContext.cs
@@ -1,13 +1,12 @@
-﻿namespace CodeFirstStudent.Data;
-
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using CodeFirstStudent.Models;
+
+namespace CodeFirstStudent.Data;
 
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
-
     public DbSet<Student> Students { get; set; }
 }
--- CodeFirstStudent/Program.cs
+++ CodeFirstStudent/Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
 using CodeFirstStudent.Data;
 
 var builder = WebApplication.CreateBuilder(args);
