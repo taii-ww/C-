0078bbe3828f037e1b2de31baa0286e039a366c0
taii-ww <tait91026@gmail.com>
2026-04-11 23:13:17 +0700

    Add products controller and CRUD views

--- DBFirstProduct/Program.cs
+++ DBFirstProduct/Program.cs
@@ -1,7 +1,13 @@
+using DBFirstProduct.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DbfirstProductContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
