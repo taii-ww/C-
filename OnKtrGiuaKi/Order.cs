b7198751e151492405128947f3eaa4dfef949795
taii-ww <tait91026@gmail.com>
2026-04-11 16:58:05 +0700

    add

---
+++ OnKtrGiuaKi/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnKtrGiuaKi
+{
+    internal class Order
+    {
+
+        public int OrderID { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int CusID { get; set; }
+    }
+}
